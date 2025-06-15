ea701528ce1b346bacfcc134460c875ee249cdee
helloworld <shchur.vadim.90@list.ru>
2025-06-15 14:31:55 +0300

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,27 @@ public class Main {
         // Задача 1;
         System.out.println("задача1");
         int[] payments = {12500, 10000, 9550, 12000, 99000};
-        int summaTrate = 0;
-        for (int vyplota : payments) {
-            summaTrate += vyplota;
+        int summaWastes = 0;
+        for (int outflow : payments) {
+            summaWastes += outflow;
         }
-        System.out.println("Сумма трат за месяц составила " + summaTrate + "рублей");
+        System.out.println("Сумма трат за месяц составила " + summaWastes + "рублей");
 
         // Задача 2;
         System.out.println("задача2");
-        int[] payments1 = Arrays.copyOf(payments, payments.length);
-        Arrays.sort( payments1);
-        int maxExpenses = payments1[payments1.length - 1];
-        int minExpenses = payments1[0];
-        System.out.println("Минимальная сумма трат за неделю составила" + minExpenses + " рублей. Максимальная сумма трат за неделю составила " + maxExpenses + "рублей.");
+        int minExpenses = payments[0];
+        int maxExpenses = payments[0];
+
+        for (int i = 1; i < payments.length; i++) {
+            if (payments[i] < minExpenses) {
+                minExpenses = payments[i];
+            }
+            if (payments[i] > maxExpenses) {
+                maxExpenses = payments[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minExpenses + " рублей. Максимальная сумма трат за неделю составила " + maxExpenses + " рублей.");
+
 
 // Задача 3;
         System.out.println("задача3");
@@ -26,13 +34,23 @@ public class Main {
         for (int expense : expenses) {
             total += expense;
         }
-        double average = total / 5.0;
+        double average = total / (double)expenses.length;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
 
 
         // Задача 4;
         System.out.println("задача4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }}}
+        int left = 0;
+        int right = reverseFullName.length - 1;
+        while (left < right) {
+            char temp = reverseFullName[left];
+            reverseFullName[left] = reverseFullName[right];
+            reverseFullName[right] = temp;
+            left++;
+            right--;
+        }
+        for (char c : reverseFullName) {
+            System.out.print(c);
+        }
+        }}
